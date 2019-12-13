0b441c9ebbd6c0b47ec3c508d9e185666704f09f
Ehsan Asgarian <ehsan.asgarian@gmail.com>
2019-12-14 00:08:40 +0330

    Add virstar example + Add result of each call

--- python-demo/python_demo.py
+++ python-demo/python_demo.py
@@ -12,7 +12,6 @@ def utfReverse(s):
     # more than 2 bytes long in utf 16
     u = s.decode("utf-8")
     return u[::-1].encode("utf-8")
-# print(utfReverse('\"من با دانشجویان دیگری برخورد کردم\"'))
 
 ######################################################################
 
@@ -26,13 +25,12 @@ def callApi(url, data, tokenKey):
         'Cache-Control': "no-cache"
     }
     response = requests.request("POST", url, data=data.encode("utf-8"), headers=headers)
-    return response.text
-    # return utfReverse(response.text.encode("utf-8"))
+    return response.text    # return utfReverse(response.text.encode("utf-8"))
     
 ##################### Get Token by Api Key ##########################
 baseUrl = "http://api.text-mining.ir/api/"
 url = baseUrl + "Token/GetToken"
-querystring = {"apikey":"YOUR_API_KEY"} 
+querystring = {"apikey":"YOUR_API_KEY"}
 response = requests.request("GET", url, params=querystring)
 data = json.loads(response.text)
 tokenKey = data['token']
@@ -41,6 +39,7 @@ tokenKey = data['token']
 url =  baseUrl + "PreProcessing/NormalizePersianWord"
 payload = u"{\"text\":\"ولــے اگــر دڪــمــه مــڪــث رو لــمــس ڪــنــیــم ڪــلــا مــتــن چــنــدیــن صــفــحــه جــابــه جــا مــیــشــه و دیــگــه نــمــیــشــه فــهمــیــد ڪــدوم آیــه تــلــاوت مــی شود بــایــد چــے ڪــنــیــم؟.\", \"refineSeparatedAffix\":true}"
 print(callApi(url, payload, tokenKey))
+# result: ولی اگر دکمه مکث رو لمس کنیم کلا متن چندین صفحه جابه جا میشه و دیگه نمیشه فهمید کدوم آیه تلاوت می‌شود باید چی کنیم؟.
 
 ##################### Call Sentence Splitter ########################
 url =  baseUrl + "PreProcessing/SentenceSplitter"
@@ -54,11 +53,18 @@ payload = u'''{\"text\": \"من با دوستم به مدرسه می رفتیم 
     \"complexSentence\": true
 }'''
 print(callApi(url, payload, tokenKey))
+# resuilt: ["من با دوستم به مدرسه می‌رفتیم","و در آنجا مشغول به تحصیل بودیم .","سپس به دانشگاه راه یافتیم"
 
 ######################## Call Tokenizer ############################
 url =  baseUrl + "PreProcessing/Tokenize"
 payload = u"\"من با دانشجویان دیگری برخورد کردم\""
 print(callApi(url, payload, tokenKey))
+# rsult: ["من","با","دانشجویان","دیگری","برخورد","کردم"]
+
+url =  baseUrl + "PreProcessing/TokenizeWithType"
+payload = u"\"اخبار 20:30 مورخ 1398/2/22 اعلام کرد شرکت T.E.T مبلغ 200.57 میلیون ارزش دارد!!!  😒 @Khabar_Alaki -- email: hi@text-mining.ir\""
+print(callApi(url, payload, tokenKey))
+#result: [{"key":"اخبار","value":"Word"},{"key":"20:30 ","value":"DateTime"},{"key":"مورخ","value":"Word"},{"key":"1398/2/22","value":"DateTime"},{"key":"اعلام","value":"Word"},{"key":"کرد","value":"Word"},{"key":"شرکت","value":"Word"},{"key":"T.E.T","value":"Abbreviation"},{"key":"مبلغ","value":"Word"},{"key":"200.57","value":"Number"},{"key":"میلیون","value":"Word"},{"key":"ارزش","value":"Word"},{"key":"دارد","value":"Word"},{"key":"!!!","value":"Separator"},{"key":"😒","value":"Emoji"},{"key":"@Khabar_Alaki","value":"SocialId"},{"key":"--","value":"Separator"},{"key":"email","value":"Word"},{"key":":","value":"Separator"},{"key":"hi@text-mining.ir","value":"Email"}]
 
 ############# Call Sentence Splitter and Tokenizer #################
 url =  baseUrl + "PreProcessing/SentenceSplitterAndTokenize"
@@ -75,27 +81,57 @@ payload = u'''{\"text\": \"من با دوستم به مدرسه می رفتیم 
     \"complexSentence\": true
 }'''
 print(callApi(url, payload, tokenKey))
+# result: [["من","با","دوستم","به","مدرسه","می‌رفتیم"],["و","در","آنجا","مشغول","به","تحصیل","بودیم","."],["سپس","به","دانشگاه","راه","یافتیم"]]
 
 ########################## Call Stemmer ##########################
 url =  baseUrl + "Stemmer/LemmatizeText2Text"
 payload = u'"من با دانشجویان دیگری برخورد کردم. سپس به آنها گفتم\nمن با شما کارهای زیادی دارم"'
 print(callApi(url, payload, tokenKey))
+''' result: 
+من با دانشجو دیگر بر خورد کرد. سپس به آن گفت
+من با شما کار زیاد داشت
+'''
 
 url =  baseUrl + "Stemmer/LemmatizePhrase2Phrase"
 payload = u'{"phrases": [{ "word": "دریانوردانی" }, { "word": "فرشتگان" }], "checkSlang": false}'
 print(callApi(url, payload, tokenKey))
+# result: [{"wordComment":"","simplePos":"","rootWords":["دریانورد","دریا"],"verbInformation":null,"sentenceNumber":0,"wordNumberInSentence":0,"startCharIndex":0,"word":"دریانوردانی","tags":{},"firstRoot":"دریانورد","wordCount":1,"length":11,"isVerb":false,"isPunc":false},{"wordComment":"","simplePos":"","rootWords":["فرشته"],"verbInformation":null,"sentenceNumber":0,"wordNumberInSentence":0,"startCharIndex":0,"word":"فرشتگان","tags":{},"firstRoot":"فرشته","wordCount":1,"length":7,"isVerb":false,"isPunc":false}]
 
 url =  baseUrl + "Stemmer/LemmatizeText2Phrase"
 payload = u'{"text": "دانشجویان زیادی به مدارس استعدادهای درخشان راه پیدا نخواهند کرد که با مشکلات بعدی مواجه شوند.", "checkSlang": false}'
 result = json.loads(callApi(url, payload, tokenKey))
 for phrase in result:
     print("("+phrase['word']+":"+phrase['firstRoot']+") ")
+''' result:
+(دانشجویان:دانشجو)
+(زیادی:زیاد)
+(به:به)
+(مدارس:مدرسه)
+(استعدادهای:استعداد)
+(درخشان:درخشان)
+(راه:راه)
+(پیدا:پیدا)
+(نخواهند کرد:نکرد)
+(که:که)
+(با:با)
+(مشکلات:مشکل)
+(بعدی:بعد)
+(مواجه:مواجه)
+(شوند:شد)
+(.:.)
+'''
 
 url =  baseUrl + "Stemmer/LemmatizeWords2Phrase"
 payload = u'["دریانوردانی", "جزایر", "فرشتگان", "تنها"]'
 result = json.loads(callApi(url, payload, tokenKey))
 for phrase in result:
     print("("+phrase['word']+":"+phrase['firstRoot']+") ")
+''' result:
+(دریانوردانی:دریانورد)
+(جزایر:جزیره)
+(فرشتگان:فرشته)
+(تنها:تنها)
+'''
 
 #################### Call Spell Corrector ########################
 url =  baseUrl + "TextRefinement/SpellCorrector"
@@ -104,6 +140,7 @@ payload = u'''{\"text\": \"فهوه با مبات میجسبد\",
             \"normalize\": true, 
             \"candidateCount\": 2}'''
 print(callApi(url, payload, tokenKey))
+# result: قهوه با {نبات,ملات} {می‌چسبد,می‌جنبد}
 
 ################ Call Spell Corrector in Context ##################
 url =  baseUrl + "TextRefinement/SpellCorrectorInContext"
@@ -111,6 +148,7 @@ payload = u'''{\"text\": \"ستر حیوانی است که در صحرا با م
             \"normalize\": true, 
             \"candidateCount\": 3}'''
 print(callApi(url, payload, tokenKey))
+# result: {شتر,سطر,سفر} {حیوانی,حیوان,یونانی} {است,دست,هست} که در {صحرا,صفرا,صدرا} با {مقدار,مدار,مقدر} کم آب {زندگی,بندگی,زدگی} {می‌کند,می‌کند,مکند}
 
 ################## Call Swear Word Detector ######################
 url =  baseUrl + "TextRefinement/SwearWordTagger"
@@ -118,6 +156,7 @@ payload = u'"خـــــــرررررهای دیووووونههه  -   صکس
 result = json.loads(callApi(url, payload, tokenKey))
 ## for item in result: ...
 print(result)
+# result: {'خرررررهای': 'MildSwearWord', 'دیووووونههه': 'MildSwearWord', 'صکس': 'StrongSwearWord', 'س.ک.س': 'StrongSwearWord', 'ی بیپدرومادر': 'StrongSwearWord'}
 
 ################ Call Slang to Formal Converter ##################
 url =  baseUrl + "TextRefinement/FormalConverter"
@@ -127,6 +166,13 @@ payload = u'''"اگه اون گزینه رو کلیک کنین، یه پنجره
     نمیدونم چی بگم که دیگه اونجا نره
     ساعت چن میتونین بیایین؟"'''
 print(callApi(url, payload, tokenKey))
+''' result:
+اگر آن گزینه را کلیک کنید، یک پنجره باز می‌شود که می‌توانید رمز عبورتان را آنجا تغییر بدهید
+داشتم می‌رفتم بروم، دیدم گرفت نشست، گفتم بگذار بپرسم ببینم می‌آید نمی‌آید دیدم می‌گوید نمی‌خواهم بیایم بگذار بروم بگیرم بخوابم نمی‌توانم بنشینم.
+کتاب‌های خودتان است
+نمی‌دانم چه بگویم که دیگر آنجا نرود
+ساعت چند می‌توانید بیایید؟
+'''
 
 ######################## Call POS-Tagger ############################
 url =  baseUrl + "PosTagger/GetPos"
@@ -134,6 +180,17 @@ payload = u'"احمد و علی به مدرسه پایین خیابان می ر�
 result = json.loads(callApi(url, payload, tokenKey))
 for phrase in result:
     print("("+phrase['word']+","+phrase['tags']['POS']['item1']+") ")
+''' result:
+(احمد,N)
+(و,CON)
+(علی,N)
+(به,P)
+(مدرسه,N)
+(پایین,ADJ)
+(خیابان,N)
+(می‌رفتند,V)
+(.,)
+'''
 
 ############################ Call NER ###############################
 url =  baseUrl + "NamedEntityRecognition/Detect"
@@ -141,25 +198,58 @@ payload = u'"احمد عباسی به تحصیلات خود در دانشگاه 
 result = json.loads(callApi(url, payload, tokenKey))
 for phrase in result:
     print("("+phrase['word']+","+phrase['tags']['NER']['item1']+") ")
+''' result:
+{احمد,B-PER}
+{عباسی,I-PER}
+{به,O}
+{تحصیلات,O}
+{خود,O}
+{در,O}
+{دانشگاه,B-ORG}
+{آزاد,I-ORG}
+{اسلامی,I-ORG}
+{در,O}
+{مشهد,I-LOC}
+{ادامه,O}
+{داد,O}
+'''
 
 ##################### Call Language Detection #######################
 url =  baseUrl + "LanguageDetection/Predict"
 payload = u'"شام ییبسن یا یوخ. سن سیز بوغازیمنان گتمیر شام. به به نه قشه یردی. ساغ اول سیز نئجه سیز. نئجه سن؟ اوشاقلار نئجه دیر؟ سلام لاری وار سیزین کی لر نئجه دیر. یاخچی"'
 print(callApi(url, payload, tokenKey))
+# result: azb
 
 ################## Call Sentiment Classification ####################
-url =  baseUrl + "SentimentAnalyzer/SentimentClassifier"
+url =  baseUrl + "SentimentAnalyzer/SentimentClassifier" # output:  0:Negative  1:Neutral  2:Positive
 payload = u"\"اصلا خوب نبود\""
-print(callApi(url, payload, tokenKey))
+print(callApi(url, payload, tokenKey)) 
+# result: 0
 
 ###################### Call Text Similarity #########################
 url =  baseUrl + "TextSimilarity/ExtractSynonyms"
 payload = u"\"احسان\""
 print(callApi(url, payload, tokenKey))
+# result: ["احسان","نیکی کردن","نیکوکاری","بخشش","نیکی","خوبی","نیکویی","انعام","نکویی","اکرام","صنع","فضل","لطف","منت","نزل","نعمت","نیکی_کردن","احسان (نام)","احسان_(نام)"]
+
+url =  baseUrl + "TextSimilarity/GetMostSimilarWord"
+payload = u'''{
+    "word": "روحانی", 
+    "topN": "50"}'''
+print(callApi(url, payload, tokenKey))
+# result: ["دولت","آقای","احمدی‌نژاد","رئیس‌جمهور","رییس‌جمهور","انتخابات","برجام","اصلاحات","خاتمی","طلبان","حسن","ترامپ","مچکرررریم","استغفار_نمیکنید","جناب","هاشمی","جهانگیری","مردم","ریاست‌جمهوری","اقتصادی","انقلاب","اختلاف_سپاه_ارتش","مجلس","سخنان","حمایت","تدبیر","سیاسی","سخنرانی","اصلاح","اصولگرایان","ظریف","اقای","انتخاباتی","مسئولین","آمریکا","شورای","جمهوری","رفسنجانی","ملت","برخروشیده‌اند","دولت_غربگرا_و_سازشگر","رهبری","جامعه","گفته","مطرح","دکتر","مناظره_روحانی_با_خودش","اصلاح‌طلب","فرصت_بیشتری","ابقای_علی_اوسط_هاشمی"]
 
 url =  baseUrl + "TextSimilarity/GetSyntacticDistance"
 payload = u'{"string1": "ایرانی ها", "string2": "ایرانیان", "distanceFunc": 2}'  # JaccardDistance
 print(callApi(url, payload, tokenKey))
+# result: 0.333333343
+
+url =  baseUrl + "TextSimilarity/GetStatisticalDistance"
+payload = u'''{
+    "string1": "روحانی", 
+    "string2": "جسمانی"}'''
+print(callApi(url, payload, tokenKey))
+# result: 0.905992568
 
 url =  baseUrl + "TextSimilarity/SentenceSimilarityBipartiteMatching"
 payload = u'''{
@@ -167,6 +257,7 @@ payload = u'''{
     "string2": "حملات مغولان به ایران", 
     "distanceFunc": 2}'''  # JaccardDistance
 print(callApi(url, payload, tokenKey))
+# result: 0.80357146263122559
 
 url =  baseUrl + "TextSimilarity/SentenceSimilarityWithIntersectionMatching"
 payload = u'''{
@@ -175,6 +266,7 @@ payload = u'''{
     "distanceFunc": 2,
     "minDistThreshold": 0.3}'''  # حداقل فاصله دو کلمه برای انطباق (یکسان فرض نمودن) آنها
 print(callApi(url, payload, tokenKey))
+# result: 0.75
 
 url =  baseUrl + "TextSimilarity/SentenceSimilarityWithNGramMatching"
 payload = u'''{
@@ -183,12 +275,14 @@ payload = u'''{
     "distanceFunc": 2,
     "minDistThreshold": 0.3}'''  # حداقل فاصله دو کلمه برای انطباق (یکسان فرض نمودن) آنها
 print(callApi(url, payload, tokenKey))
+# result: 0.714285708963871
 
 url =  baseUrl + "TextSimilarity/SentenceSimilarityWithNearDuplicateDetector"
 payload = u'''{
     "string1": "حمله مغولها به ایران", 
     "string2": "حملات مغولان به ایران"}'''
 print(callApi(url, payload, tokenKey))
+# result: 0.5
 
 ################ Call Information Retrieval Function(s) ##################
 url =  baseUrl + "InformationRetrieval/KeywordExtraction"
@@ -203,3 +297,37 @@ payload = u'''{
     "resultKeywordCount": 5,
     "method": "TFIDF"}'''
 print(callApi(url, payload, tokenKey))
+''' result:
+{
+    "امارات": 75.26119828977285,
+    "توسعه انرژی": 23.99621570204974,
+    "۷۰۰ میلیون درهم": 21.95849183879027,
+    "مناطق شمالی": 20.25414682800825,
+    "سرمایه‌گذاری هنگفت امارات": 17.670494399999686
+}
+'''
+
+url =  baseUrl + "InformationRetrieval/StopWordRemoval"
+payload = u'''"تیم متن کاوی فارسی‌یار با مجموعه‌ای از فارغ التحصیلان دانشگاه‌های صنعتی شریف، تربیت مدرس و فردوسی مشهد از سال ۱۳۹۰ بصورت تخصصی در زمینه پردازش زبان طبیعی مشغول به فعالیت است. در سال ۱۳۹۶ در جهت فعالیت پژوهشی عمیق­‌تر در زمینه پردازش متون برای زبان فارسی، این گروه با آزمایشگاه متن کاوی و یادگیری ماشین پژوهشگاه علوم و فناوری اطلاعات ایران (ایرانداک) همکاری تنگاتنگی داشته است."'''
+print(callApi(url, payload, tokenKey))
+# result: تیم متن کاوی فارسی‌یار مجموعه‌ای فارغ التحصیلان دانشگاه‌های صنعتی شریف، تربیت مدرس فردوسی مشهد سال ۱۳۹۰ تخصصی پردازش زبان طبیعی مشغول فعالیت. سال ۱۳۹۶ فعالیت پژوهشی عمیق‌تر پردازش متون زبان فارسی، گروه آزمایشگاه متن کاوی یادگیری ماشین پژوهشگاه علوم فناوری اطلاعات ایران (ایرانداک) همکاری تنگاتنگی.
+
+######################## Call Virastar ############################
+url =  baseUrl + "Virastar/ScanText"
+payload = u'{"text": "حتما آن ها مومن را احترام مے ڪنند. یگ بپنجره ی بزگ وسبز باز میشود . !حضور تان را کرامی می داشتم", "returnOnlyChangedTokens": false}'
+result = json.loads(callApi(url, payload, tokenKey))
+output = ''
+for token in result:
+    output += token['originalText']
+    if (token['isChanged']):
+        output += "{" + token['editList'][0]['suggestedText'] + "(" + token['editList'][0]['description'] + ")"
+        if(len(token['editList']) > 1):
+            iterEdit = iter(token['editList'])
+            next(iterEdit)
+            for edit in iterEdit:
+                output += " - " + edit['suggestedText'] + "(" + edit['description'] + ")"
+        output += "}"
+print(output)
+''' result:
+حتما{حتماً(اصلاح تنوین)} آن ها{آن‌ها(اصلاح پسوند)} مومن{مؤمن(اصلاح حروف دارای همزه)} را احترام مے ڪنند{می‌کنند(اصلاح پیشوند، اصلاحات نویسه‌ها)}. یگ{یک(اصلاح اشتباه تایپی/املائی رایج)} بپنجره ی{بپنجره‌ی(اصلاح پسوند) - به پنجره‌ی(جداسازی واژه‌های بهم چسبیده)} بزگ{بزرگ(اصلاح اشتباه تایپی/املائی رایج)} وسبز{و سبز(جداسازی واژه‌های بهم چسبیده)} باز میشود{می‌شود(اصلاح پیشوند)} . !{.! (اصلاح فاصله‌گذاری مطابق دستور زبان)}حضور تان{حضورتان(پیشنهاد پیوسته‌نویسی واژه‌های چندبخشی)} را کرامی{کرمی(این واژه از نظر لغوی مشابه «کرمی» است. (با میزان فاصلۀ: 1)) - رامی(این واژه از نظر لغوی مشابه «رامی» است. (با میزان فاصلۀ: 0.98)) - گرامی(این واژه از نظر لغوی مشابه «گرامی» است. (با میزان فاصلۀ: 0.75))} می داشتم{می‌داشتم(اصلاح پیشوند)}
+'''
